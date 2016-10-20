4169e459717815778600ebe15622a3569ea431a6
Xianyang <Chester@MacBook-2.local>
2016-10-21 00:33:13 +0800

    update UI

--- Valet_Parking_ValetSide/Valet_Parking_ValetSide/HttpClient.m
+++ Valet_Parking_ValetSide/Valet_Parking_ValetSide/HttpClient.m
@@ -9,8 +9,8 @@
 #import "HttpClient.h"
 #import <AFNetworking/AFNetworking.h>
 
-static NSString * const kIPAdress = @"http://147.8.232.248:3001/";
-//static NSString * const kIPAdress = @"http://192.168.1.106:3001/";
+//static NSString * const kIPAdress = @"http://147.8.232.248:3001/";
+static NSString * const kIPAdress = @"http://192.168.1.106:3001/";
 
 @interface HttpClient()
 
